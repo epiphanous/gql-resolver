804d129ff0e4154097be46416f02eaf616961e25
Milan Velebit <velebit@accusys.co>
2018-11-15 16:29:30 +0100

    Added constructors to gqlpatterns

--- src/models/GQLPattern.ts
+++ src/models/GQLPattern.ts
@@ -4,12 +4,23 @@ import {GQLFieldBooster} from './GQLFieldBooster';
 
 export class GQLPattern extends Record<GQLPattern> {
     public field: string;
+
+    constructor(fieldarg: string) {
+        this.field = fieldarg;
+    }
 }
 
 export class GQLGeoNearFeaturePattern extends GQLPattern {
     public feature: string;
     public distance: number;
     public units: string;
+
+    constructor(fieldarg, feature, units: string, distance: number) {
+        super(fieldarg);
+        this.feature = feature;
+        this.distance = distance;
+        this.units = units;
+    }
 }
 
 export class GQLGeoNearLatLonPattern extends GQLPattern {
@@ -17,6 +28,14 @@ export class GQLGeoNearLatLonPattern extends GQLPattern {
     public lon: string;
     public distance: number;
     public units: string;
+
+    constructor(fieldarg, lat, lon, units: string, distance: number) {
+        super(fieldarg);
+        this.lat = lat;
+        this.lon = lon;
+        this.units = units;
+        this.distance = distance;
+    }
 }
 
 export class GQLTextMatchPattern extends GQLPattern {
@@ -25,4 +44,13 @@ export class GQLTextMatchPattern extends GQLPattern {
     public boost: GQLFieldBooster;
     public minScore: Option<number>;
     public maxHits: Option<number>;
+
+    constructor(fieldarg, text: string, boost: GQLFieldBooster, isGeo: boolean, minScore, maxHits: Option<number>) {
+        super(fieldarg);
+        this.isGeo = isGeo;
+        this.text = text;
+        this.boost = boost;
+        this.minScore = minScore;
+        this.maxHits = maxHits;
+    }
 }
