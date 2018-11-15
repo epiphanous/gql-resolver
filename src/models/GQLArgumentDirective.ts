304b61f22c825a70cd7ad0f1df8571bb52494e56
Robert Lyons <nextdude@gmail.com>
2018-11-15 06:26:41 -0500

    fix GQLDirective hierarchy (#3)

--- src/models/GQLArgumentDirective.ts
+++
@@ -1,6 +0,0 @@
-import { GQLArgument } from './GQLArgument';
-import { GQLDirective } from './GQLDirective';
-
-export class GQLArgumentDirective<T> extends GQLDirective {
-  public argument: GQLArgument<T>;
-}
