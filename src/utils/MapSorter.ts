20c6bbdfe37bfa9576c746554d2c41ebc101d7a5
Robert Lyons <nextdude@gmail.com>
2018-11-19 10:11:34 -0500

    add testing (#11)

--- src/utils/MapSorter.ts
+++ src/utils/MapSorter.ts
@@ -1,22 +1,19 @@
-import {List, Map, OrderedMap} from 'immutable';
+import { List, Map, OrderedMap } from 'immutable';
 
 export function sortMapByProjectionOrder<K, V>(
   map: Map<K, V>,
-  projectionOrder: List<K>,
+  projectionOrder: List<K>
 ): OrderedMap<K, V> {
   const missing = map
     .keySeq()
     .toOrderedSet()
     .sort()
     .subtract(projectionOrder);
-  const completeProjectionOrder = projectionOrder
-    .asMutable()
-    .concat(missing)
-    .toList();
+  const completeProjectionOrder = projectionOrder.concat(missing).toList();
   return map
     .sortBy(
       (value, key) => completeProjectionOrder.indexOf(key),
-      (a, b) => (a > b ? 1 : a < b ? -1 : 0),
+      (a, b) => (a > b ? 1 : a < b ? -1 : 0)
     )
     .toOrderedMap();
 }
