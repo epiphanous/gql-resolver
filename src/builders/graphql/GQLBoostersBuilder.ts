652680909318a9f1309d41bd37731d06c18453d2
Milan Velebit <velebit@accusys.co>
2018-11-30 12:08:15 +0100

    GQLBoostersBuilder added

---
+++ src/builders/graphql/GQLBoostersBuilder.ts
@@ -0,0 +1,61 @@
+import {Parser} from 'antlr4ts';
+import {List, Map, Set} from 'immutable';
+import * as QMP from '../../antlr4/generated/QueryModificationParser';
+import {GQLBooster, GQLUserFollowsBooster, GQLUserIsFollowedBooster} from '../../models/GQLBooster';
+import {GQLVariableDefinition} from '../../models/GQLVariableDefinition';
+import GQLObjectQueryModifierBuilder from './GQLObjectQueryModifierBuilder';
+
+export default class GQLBoostersBuilder extends GQLObjectQueryModifierBuilder {
+    public result: List<GQLBooster>;
+    constructor(
+        validFields: Map<string, string>,
+        validVariables: Set<GQLVariableDefinition>,
+        vars: Map<string, any>,
+        prefixes: Set<string>,
+        source: string = 'bindings'
+    ) {
+        super(
+            validFields,
+            validVariables,
+            vars,
+            prefixes,
+            source
+        );
+    }
+
+    public parse(parser: Parser) {
+        return (parser as QMP.QueryModificationParser).boosters();
+    }
+
+    public exitBoosters(context: QMP.BoostersContext) {
+        this.result = this.processBoosters(context);
+    }
+
+    public processBoosters(context: QMP.BoostersContext) {
+        return List(context.boost()).map(a => this.processBooster(a));
+    }
+
+    /**
+     * I'll leave the method here even though we might not need these statements inside
+     */
+    public processBooster(context: QMP.BoostContext) {
+        if (context instanceof QMP.FollowsUserBoostContext) {
+            return this.processFollowsUser(context);
+        }
+        if (context instanceof QMP.FollowedByUserBoostContext) {
+            return this.processedFollowedByUser(context);
+        }
+    }
+
+    public processFollowsUser(context: QMP.FollowsUserBoostContext) {
+        const boost = this.processNumericLiteral(context.numericLiteral()).expression;
+        const user = Number(this.processIriRefOrVarRef(context.iriRefOrVarRef()).expression);
+        return new GQLUserIsFollowedBooster(user, boost);
+    }
+
+    public processedFollowedByUser(context: QMP.FollowedByUserBoostContext) {
+        const boost = this.processNumericLiteral(context.numericLiteral()).expression;
+        const user = Number(this.processIriRefOrVarRef(context.iriRefOrVarRef()).expression);
+        return new GQLUserFollowsBooster(user, boost);
+    }
+}
