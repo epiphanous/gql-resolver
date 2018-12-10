1e507c4b9fbba75f9d68967e5328e3e919f9e8a9
Milan Velebit <velebit@accusys.co>
2018-12-10 16:31:07 +0100

    Fixed the BuilderErrorListener

--- src/builders/BuilderErrorListener.ts
+++ src/builders/BuilderErrorListener.ts
@@ -1,16 +1,24 @@
+import {RecognitionException} from 'antlr4ts';
 import {ANTLRErrorListener} from 'antlr4ts/ANTLRErrorListener';
+import {Option} from 'funfix';
 import BuilderBase from './BuilderBase';
 import BuilderError from './BuilderError';
 
 export default class BuilderErrorListener<T> implements ANTLRErrorListener<any> {
   public builder: BuilderBase<T>;
-  constructor(builder) {
+  constructor(builder: BuilderBase<T>) {
     this.builder = builder;
   }
 
-  public syntaxError(parser, offendingSymbol, line, position, message, exception) {
+  public syntaxError(parser: any,
+                     offendingSymbol: any,
+                     line: number,
+                     position: number,
+                     message: string,
+                     exception: RecognitionException
+  ) {
     this.builder.addError(
-      new BuilderError({ line, position, message, exception }),
+      new BuilderError(message, line, position, Option.of(exception)),
     );
   }
 }
