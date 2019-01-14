d461515da1b534cb0eb2721b6e55592812e8009f
Milan Velebit <velebit@accusys.co>
2019-01-14 15:51:51 +0100

    operationName fallback fix

--- src/Resolver.ts
+++ src/Resolver.ts
@@ -22,7 +22,8 @@ export class Resolver {
     console.log(`Query ${q}, vars: ${vars}, operation:${operation.value}`);
     const builder = new GQLQueryBuilder(this.context, vars);
     return Builder.parse<GQLQueryDocument>(builder, q).flatMap(doc => {
-      const opName = operation.getOrElse(doc.operations.first());
+      const firstOp: GQLOperation = doc.operations.first();
+      const opName = operation.getOrElse(firstOp.name);
       console.log('OPERATION NAME: ', opName);
       return this.executeOperation(
         Option.of(
