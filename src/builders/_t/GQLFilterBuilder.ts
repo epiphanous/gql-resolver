28cb884b96247aa42474f4f122168b85cc69fc49
Robert Lyons <nextdude@gmail.com>
2019-03-14 08:45:49 -0400

    update test

--- src/builders/_t/GQLFilterBuilder.ts
+++ src/builders/_t/GQLFilterBuilder.ts
@@ -1,20 +1,32 @@
 import { assert, expect } from 'chai';
 import fs = require('fs');
-import GQLFilterBuilder from '../graphql/GQLFilterBuilder';
+import { Map, Set } from 'immutable';
+import { GQLFilter } from '../../models/GQLFilter';
 import ResolverContext from '../../models/ResolverContext';
-import {Map, Set} from 'immutable';
-import {GQLFilter} from '../../models/GQLFilter';
+import GQLFilterBuilder from '../graphql/GQLFilterBuilder';
+import { GQLObjectType } from '../../models/GQLTypeDefinition';
 
 describe('GQLFilterBuilder test', () => {
-  const schema = ResolverContext.buildSchema(fs.readFileSync('./src/schema.graphql', 'utf8'));
+  const schema = ResolverContext.buildSchema(
+    fs.readFileSync('./src/schema.graphql', 'utf8')
+  );
   it('produces a valid filter object', async () => {
-    const gnFeatureTypeDef = schema.getTypeDefinition('gn_Feature');
-    const filterString = `s_name = 'zomg'`;
-    const validFields = gnFeatureTypeDef.value.fields.reduce((acc, gqlfd) => {
-      return acc.set(gqlfd.name, gqlfd.gqlType.name);
-    }, Map<string, string>().asMutable());
-    const GQLFilterObject = new GQLFilterBuilder(validFields, Set(), Map<string, any>(), Set(), filterString);
-    console.log('done', GQLFilterObject);
+    const gnFeature = schema
+      .getTypeDefinition('gn_Feature')
+      .get() as GQLObjectType;
+    const filterString = "s_name = 'zomg'";
+    const validFields = Map(
+      gnFeature.fields.map<[string, string]>(fd => [fd.name, fd.gqlType.name])
+    );
+    console.log(validFields.toJS());
+    const GQLFilterObject = new GQLFilterBuilder(
+      validFields,
+      Set(),
+      Map<string, any>(),
+      Set(),
+      filterString
+    );
+    console.log('done', GQLFilterObject.result);
     expect(GQLFilterObject.result).to.be.instanceof(GQLFilter);
   });
 });
